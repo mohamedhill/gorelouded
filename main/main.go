a789e917d120146c856eb62ea8039ea9009162c9
Your Name <you@example.com>
2025-06-29 13:25:47 +0100

    go-relouded

--- main/main.go
+++ main/main.go
@@ -10,23 +10,23 @@ import (
 func main() {
 	filenames := os.Args
 	if len(filenames) < 3 {
-		fmt.Println("Usage: program inputfile outputfile")
+		fmt.Println("program inputfile:",filenames[1],"||", "outputfile: ??")
 		return
 	}
-	if goreloaded.ProtectedFile(filenames[0], filenames[1]) {
-		fmt.Println("This program only works with 'sample.txt' and 'result.txt'.", filenames)
+	if !goreloaded.ProtectedFile(filenames[0], filenames[1]) {
+		fmt.Println("This program only works with 'sample.txt' and 'result.txt'not:", filenames[1:])
 		return
 	}
 	data, err := os.ReadFile(filenames[1])
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Println("program can't read  input file>>:", err)
 		return
 	}
 	clean := goreloaded.CleanStr(string(data))
-
 	zrox := goreloaded.Gorseloaded(clean)
 	err = goreloaded.WriteOutput(filenames[2], zrox)
 	if err != nil {
-		fmt.Println("Error writing output:", err)
+		fmt.Println("program can't write output file :", err)
+		return
 	}
 }
